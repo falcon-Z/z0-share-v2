aaf9734afda57954f3abc4bb8c746819b60b7bd1
sai madhan <saimadhan@outlook.in>
2024-08-21 20:22:10 +0530

    starter

---
+++ src/components/auth/register.tsx
@@ -0,0 +1,7 @@
+export default function Registation() {
+    return (
+        <div>
+            <h1>Registation</h1>
+        </div>
+    )
+}   
---
+++ src/pages/auth.tsx
@@ -0,0 +1,9 @@
+import AuthCard from "@falcon-z/components/auth/authCard";
+
+export default function AuthPage() {
+  return (
+    <div className="grid h-screen w-full place-items-center">
+      <AuthCard />
+    </div>
+  );
+}
